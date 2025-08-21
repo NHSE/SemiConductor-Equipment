483e4be20714d0062e6691e9468dcbf17859f8fe
NHSeok <dlfnqwe159@gmail.com>
2025-08-21 16:25:32 +0900

    fix : 1. Build 오류 제거(Resource 변경 후 재 Build)

--- SemiConductor Equipment/obj/Debug/net9.0-windows/App.g.i.cs
+++ SemiConductor Equipment/obj/Debug/net9.0-windows/App.g.i.cs
@@ -74,7 +74,7 @@ namespace SemiConductor_Equipment {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/SemiConductor Equipment;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SemiConductor Equipment;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
